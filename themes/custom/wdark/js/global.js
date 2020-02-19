d219616a1528bafb4ff73c5b020cd4d2a1c649f7
Wdark <wdark@Wolfred-2.local>
2020-02-18 22:22:11 -0500

    loader appear

--- themes/custom/wdark/js/global.js
+++ themes/custom/wdark/js/global.js
@@ -6,7 +6,27 @@
 (function ($, Drupal) {
 
   'use strict';
+  var preloader;
 
+  function preload(opacity) {
+    if (opacity <= 0) {
+      showContent();
+    }
+    else {
+      preloader.style.opacity = opacity;
+      window.setTimeout(function () { preload(opacity - 0.05) }, 100);
+    }
+  }
+
+  function showContent() {
+    preloader.style.display = 'none';
+    document.getElementById('page-wrapper').style.display = 'block';
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    preloader = document.getElementById('preloader');
+    preload(1);
+  });
   Drupal.behaviors.wdark = {
     attach: function (context, settings) {
 
