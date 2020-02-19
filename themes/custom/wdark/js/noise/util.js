d1081959394fa9771e386d86b376a5597e352ab4
Wdark <wdark@Wolfred-2.local>
2020-02-18 23:37:01 -0500

    minify items

--- themes/custom/wdark/js/noise/util.js
+++ themes/custom/wdark/js/noise/util.js
@@ -1,17 +1,1 @@
-const { PI, cos, sin, abs, sqrt, pow, round, random, atan2 } = Math;
-const HALF_PI = 0.5 * PI;
-const TAU = 2 * PI;
-const TO_RAD = PI / 180;
-const floor = n => n | 0;
-const rand = n => n * random();
-const randIn = (min, max) => rand(max - min) + min;
-const randRange = n => n - rand(2 * n);
-const fadeIn = (t, m) => t / m;
-const fadeOut = (t, m) => (m - t) / m;
-const fadeInOut = (t, m) => {
-	let hm = 0.5 * m;
-	return abs((t + hm) % m - hm) / (hm);
-};
-const dist = (x1, y1, x2, y2) => sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-const angle = (x1, y1, x2, y2) => atan2(y2 - y1, x2 - x1);
-const lerp = (n1, n2, speed) => (1 - speed) * n1 + speed * n2;
+const { PI: PI, cos: cos, sin: sin, abs: abs, sqrt: sqrt, pow: pow, round: round, random: random, atan2: atan2 } = Math, HALF_PI = .5 * PI, TAU = 2 * PI, TO_RAD = PI / 180, floor = a => 0 | a, rand = a => a * random(), randIn = (a, n) => rand(n - a) + a, randRange = a => a - rand(2 * a), fadeIn = (a, n) => a / n, fadeOut = (a, n) => (n - a) / n, fadeInOut = (a, n) => { let r = .5 * n; return abs((a + r) % n - r) / r }, dist = (a, n, r, d) => sqrt(pow(r - a, 2) + pow(d - n, 2)), angle = (a, n, r, d) => atan2(d - n, r - a), lerp = (a, n, r) => (1 - r) * a + r * n;
